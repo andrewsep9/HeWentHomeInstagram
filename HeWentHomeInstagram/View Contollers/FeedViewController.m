a2825f56a238648833a5ecb3bc44eee7d913bea9
Andrew Sepúlveda <andrews9@fb.com>
2019-07-09 15:30:49 -0700

    can logout, still haven't made UI look pretty

--- HeWentHomeInstagram/View Contollers/FeedViewController.m
+++ HeWentHomeInstagram/View Contollers/FeedViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "FeedViewController.h"
+#import "User.h"
+#import <Parse/Parse.h>
+#import "PFUser.h"
+#import "LoginViewController.h"
+#import "AppDelegate.h"
 
 @interface FeedViewController ()
 
@@ -19,14 +24,24 @@
     // Do any additional setup after loading the view.
 }
 
-/*
+- (IBAction)logoutTapped:(UIBarButtonItem *)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+}
+
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+    
 }
-*/
+
 
 @end
